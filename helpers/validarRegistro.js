ff2eef58fc1cd4ae1e2e900c6938e0a96297631f
Pao-Molina <pmova13@gmail.com>
2025-09-13 14:53:32 -0500

    (Develop): Inicio de Validacion con arreglos regulares

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,13 @@
+//let inputs = document.querySelectorAll("form div inputs") Selecciona todos los inputs del formlario
+//let inputs = document.querySelectorAll(".formulario_entrada")
+//inputs.map((input)=> {})  Sintaxis Arreglo.Metodo (inputs.map) la variable toma el nombre en singular del arreglo
+ 
+let inputs = document.querySelectorAll(".formulario_entrada")
+    let arregloInputs = [...inputs]
+    console.log(input)
+    console.log(arregloInputs)
+
+arregloInputs.map((input) =>{
+    console.log(input)
+})
+
